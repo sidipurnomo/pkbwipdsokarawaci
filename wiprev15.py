1fbb32f7c45b79e93c0264faeca260dfb0a8d8c3
sidipurnomo <sidi.purnomo87@gmail.com>
2026-07-13 18:04:31 +0700

    Update WA number for SAHRIM022761

--- wiprev15.py
+++ wiprev15.py
@@ -32,7 +32,7 @@ WA_API_TOKEN = "CIgRwaeFa1cvnYaWH1RtBL6taXQi3vcq"
 # [PENTING] Silakan isi dan sesuaikan NAMA SA beserta Nomor WA-nya di sini.
 # Pastikan ejaan nama sesuai dengan yang ada di Google Sheets (huruf besar/kecil otomatis disesuaikan)
 WA_SA_MAP = {
-    "SAHRIM022761": "6287774134574",
+    "SAHRIM022761": "6281287200880",
     "MAULAN030509": "6281366664391",
     "BERLIA039884": "6283893470438",
     "MUHAMM086163": "628558825962",
